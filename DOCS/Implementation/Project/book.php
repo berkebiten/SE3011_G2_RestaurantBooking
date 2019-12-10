b0200d0cb71d5bdd44b9d4a01a63921acf41ab63
Ali Atahan Topal <atahanali57@gmail.com>
2019-12-11 00:04:02 +0300

    book

--- DOCS/Implementation/Project/book.php
+++ DOCS/Implementation/Project/book.php
@@ -1,27 +1,47 @@
 <!DOCTYPE html>
 <link rel="stylesheet" href="style.css"></link>
 <script src="scripts.js"></script>
-<html>
-    <body>
-        REZERVASYONUNUZ BASARIYLA TAMAMLANMISTIR. AFIYET OLSUN :)
-    </body>
-</html>
 <?php
 include('session.php');
 include("dbconnect.php");
-
-
 $party = filter_input(INPUT_POST, 'party');
 $c_username = $_SESSION['username'];
-$r_username = filter_input(INPUT_POST, 'rName');
+$r_username = $_SESSION['restaurant_name'];
 $date = filter_input(INPUT_POST, 'date');
-$time = filter_input(INPUT_POST, 'time');
+$startTime = filter_input(INPUT_POST, 'startTime');
+$endTime = filter_input(INPUT_POST, 'endTime');
 $phone =filter_input(INPUT_POST, 'phoneNo');
 $fname =filter_input(INPUT_POST, 'fname');
 $lname =filter_input(INPUT_POST, 'lname');
 $email =filter_input(INPUT_POST, 'email');
 
-echo $c_username;
+$query1 = mysqli_query($conn, "SELECT restaurant_uname, start_time, end_time, date, party FROM bookings WHERE restaurant_uname = '$r_username' AND date = '$date'");
+$array = array();
+$array[] = mysqli_fetch_array($query1, MYSQLI_ASSOC);
+$count = 0;
 
-$query = mysqli_query($conn, "insert into bookings VALUES('$c_username', '$r_username','$party','$time','$fname','$lname','$email','$phone','$date')" );
+for($i = 0; $i < sizeof($array); $i++){
+    echo $array[$i];
+    if($array['end_time'] <= $startTime && $array['start_time'] >= $endTime){
+        unset($array[$i]);
+    }
+}
+
+//while($array){
+//    if($array['end_time'] <= $startTime && $array['start_time'] >= $endTime){
+//        
+//    }
+//}
+
+
+$query2 = mysqli_query($conn, "SELECT * FROM bookings WHERE end_time <= $startTime OR start_time >= $endTime");
+
+//$query = mysqli_query($conn, "insert into bookings VALUES('$c_username', '$r_username','$party','$startTime','$endTime','$fname','$lname','$email','$phone','$date')" );
   ?>
+
+
+<html>
+    <body>
+        REZERVASYONUNUZ BASARIYLA TAMAMLANMISTIR. AFIYET OLSUN :)
+    </body>
+</html>
