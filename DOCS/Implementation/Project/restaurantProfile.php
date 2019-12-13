30c92f937d1255086fb5990526b7837369f6eacf
OguzKaanYazan <56766209+OguzKaanYazan@users.noreply.github.com>
2019-12-13 23:27:13 +0300

    Update restaurantProfile.php

--- DOCS/Implementation/Project/restaurantProfile.php
+++ DOCS/Implementation/Project/restaurantProfile.php
@@ -19,6 +19,9 @@ $count = mysqli_num_rows($query);
 if ($count == 0) {
     header('location:errorPage.php');
 }
+    $restImg = mysqli_query($conn, "SELECT * FROM images WHERE uname='$uname'");
+    $imgArr = mysqli_fetch_assoc($restImg);
+    $count2 = mysqli_num_rows($restImg);
 ?>
 <html>
     <head>
@@ -50,34 +53,22 @@ if ($count == 0) {
             <div id="secondPart">
                 <div class="slideshow-container">
 
-                    <div class="mySlides fade">
-                        <div class="numbertext">1 / 3</div>
-                        <img class="restPics" src="restaurantImages/rest1/1.jpg">
-                        <div class="text">Caption Text</div>
-                    </div>
-
-                    <div class="mySlides fade">
-                        <div class="numbertext">2 / 3</div>
-                        <img class="restPics" src="img/2.jpg">
-                        <div class="text">Caption Two</div>
-                    </div>
-
-                    <div class="mySlides fade">
-                        <div class="numbertext">3 / 3</div>
-                        <img class="restPics" src="img/3.jpg">
-                        <div class="text">Caption Three</div>
-                    </div>
-
-                    <!-- Next and previous buttons -->
-                    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-                    <a class="next" onclick="plusSlides(1)">&#10095;</a>
-                </div>
-                <br>
-                <div style="text-align:center">
-                    <span class="dot" onclick="currentSlide(1)"></span>
-                    <span class="dot" onclick="currentSlide(2)"></span>
-                    <span class="dot" onclick="currentSlide(3)"></span>
-                </div>
+                   <?php while ($imgArr = mysqli_fetch_array($restImg, MYSQLI_ASSOC)) : ?>
+            <div class="mySlides fade">
+                <img class="restPics" src="restaurantImages/<?php echo $uname?>/<?php echo $imgArr['name']?>">
+                    <div class="text"><?php echo $count2 ?></div>
+            </div>
+             <?php endwhile; ?> 
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+        </div>
+        <br>
+       <div style="text-align:center">
+            <span class="dot" onclick="currentSlide(1)"></span>
+            <span class="dot" onclick="currentSlide(2)"></span>
+            <span class="dot" onclick="currentSlide(3)"></span>
+        </div>
+                
                 <?php
                 echo "<a href='img.php?varname=$uname'>Upload a photo </a>";
                 ?>
