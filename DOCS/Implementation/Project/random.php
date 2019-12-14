123021b5d1b3068edd66f388fa5d422d47093287
OguzKaanYazan <56766209+OguzKaanYazan@users.noreply.github.com>
2019-12-14 11:13:08 +0300

    xx

---
+++ DOCS/Implementation/Project/random.php
@@ -0,0 +1,6 @@
+<?php
+function generateRandomString($length = 10) {
+    return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
+}
+
+echo  generateRandomString();  
