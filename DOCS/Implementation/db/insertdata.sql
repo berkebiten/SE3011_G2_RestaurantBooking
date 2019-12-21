6d548b6ce2e9b999e60f9f911d32680f8db4e60f
berkebiten <xqaesocial@gmail.com>
2019-12-21 20:23:12 +0300

    db queries

---
+++ DOCS/Implementation/db/insertdata.sql
@@ -0,0 +1,63 @@
+use rbsdb;
+INSERT INTO `user` VALUES ('applejobs','steve','jobs','apple@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','KpBIXZqo',0,0);
+INSERT INTO `user` VALUES ('berbuser','Berke','Biten','xqaesocial@gmail.com','d2711a679e1c05203da9a7b2133e8c45','yDMXlcVE',0,0);
+INSERT INTO `user` VALUES ('billgates','Bill','Gates','microsoft@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','GoajtYwX',0,0);
+INSERT INTO `user` VALUES ('bitenov','berbino','berke','adsadsda@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','tZSnxfBc',0,0);
+INSERT INTO `user` VALUES ('bluffist43','aral','karaoglan','bluffist@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','3iTIbwVA',0,0);
+INSERT INTO `user` VALUES ('darthali','ali','topal','darth@gmail.com','668aa797984456095a3a17a0d3f384c5','eZ9vzGL4',0,0);
+INSERT INTO `user` VALUES ('dasdasdqw','qweqwewqe','qwewqeqwq','gmoko@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','Yg1LwTrZ',0,0);
+INSERT INTO `user` VALUES ('elifdeniz','Deniz','Bolukbasi','elifdeniz@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','2IQOaRDJ',0,0);
+INSERT INTO `user` VALUES ('emineekin','Emine','Ekin','emine@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','dahqN2Kp',0,0);
+INSERT INTO `user` VALUES ('fabfabulous','asim','cihat','fabfab@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','EhkM9tU0',0,0);
+INSERT INTO `user` VALUES ('facebook','Mark','Zuckerberg','mark@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','Tqv53f4P',0,0);
+INSERT INTO `user` VALUES ('grayson34','atakan','unlu','grayson34@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','mrDcR3nB',0,0);
+INSERT INTO `user` VALUES ('hulyakocer','hulya','kocer','hulya@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','63XEnJ9I',0,0);
+INSERT INTO `user` VALUES ('irohtsm1','omer','develi','iroh@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','LTOBJiHr',0,0);
+INSERT INTO `user` VALUES ('izmitpismaniye','kocaeli','izmit','okoko@gmail.com','466b89dbbbbf0c3455bb269957a678eb','Rlbc1hDA',0,0);
+INSERT INTO `user` VALUES ('kalimdor35','onur','buyukbas','kalimdor@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','vmucjgUw',0,0);
+INSERT INTO `user` VALUES ('kelbero','Berat','Karacalar','berobero12@gmail.com','f71a2b924dcc515c3d227f1a8f33c05c','EFGAodnM',0,0);
+INSERT INTO `user` VALUES ('klein1324','berke','karadumanbiten','klein@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','Hrs7YWe6',0,0);
+INSERT INTO `user` VALUES ('meltemdeniz','Meltem','Deniz','meltem@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','uQCXHhOf',0,0);
+INSERT INTO `user` VALUES ('mrkrc3454','emre','karacalar','mrkrc@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','uE6Rjnka',0,0);
+INSERT INTO `user` VALUES ('nitoets79','kutay','buyuk','nitoets@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','M1Y5njSg',0,0);
+INSERT INTO `user` VALUES ('orhanpamuk','Orhan','Pamuk','orhan@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','a6Et3zNG',0,0);
+INSERT INTO `user` VALUES ('pankylol','ugur','tas','panky@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','mZEBGFed',0,0);
+INSERT INTO `user` VALUES ('phybarin3','ertug','dilek','phybarin@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','0LpNMc5W',0,0);
+INSERT INTO `user` VALUES ('potizmit','potrok','izmit','kokoko@gmail.com','f728dc959d5eaccaed4b9dd60acbe85c','UeC6zBgJ',0,0);
+INSERT INTO `user` VALUES ('potwor13','berke','karaduman','potworbeast@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','xT3piWJG',0,0);
+INSERT INTO `user` VALUES ('scofield34','berke','erturk','scofield@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','tXjbmE7r',0,0);
+INSERT INTO `user` VALUES ('shakzy414','omer','donmez','shakzy@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','j678Mos3',0,0);
+INSERT INTO `user` VALUES ('stomaged5','furkan','okcu','stomaged@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','XfkmI5OA',0,0);
+INSERT INTO `user` VALUES ('stoppers45','kutay','dalkilic','stoppers@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','ckaL2AqF',0,0);
+INSERT INTO `user` VALUES ('tekbeans','boray','tek','tekbeans@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','xOTpA37d',0,0);
+INSERT INTO `user` VALUES ('tugbakoc','tugba','koc','tugba@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','1YIK56up',0,0);
+INSERT INTO `user` VALUES ('utheneras','sukru','senturk','uthenera@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','AOuHSK5T',0,0);
+INSERT INTO `user` VALUES ('zeitnotdunya','berkay','asikuzun','zeitnot@gmail.com','d370b5a1ae62abeddb97ac4c91b6cae0','Ke3TCxA2',0,0);
+
+INSERT INTO `restaurant_owner` VALUES('lapulga', 'Lionel', 'Messi','Arjantin Midye','messigoat@gmail.com','c48441e989575be5b9c68af087510134','Umraniye','33724442',60,'Arjantinin temiz denizlerinden çıkmış midyeler su fiyatına gidiyor(Brezilyada daha pahalı)','AMEX, Diners Club, MasterCard, Visa','Midyeler denizden yeni cikma','Asagi Dudullu','8:00:00','24:00:00',3,'Acili','Acik Hava',1,0,0,'3iTIbwVA');
+INSERT INTO `restaurant_owner` VALUES('kingjames', 'Lebron', 'James','Taco Tuesday','lalakers@gmail.com','c48441e989575be5b9c68af087510134','Uskudar','33727742',100,'Yo whatup these tacos are so yummy and cheap','MasterCard, Visa','Tacolar cok guzeldi','Fistikagaci','10:00:00','22:00:00','2','Hot Tacos Good Tacos','Bar/Lounge, Beer, Cocktails, Corkage Fee,','2',0,0,'yDMXlcVE');
+INSERT INTO `restaurant_owner` VALUES('allfather', 'Odin', 'Borson','Iskandinav Kebap','valhalla@gmail.com','c48441e989575be5b9c68af087510134','Bagcilar','33728742',20,'Iskandinav lezzeti bir baska oluyor.','MasterCard, Visa','Iskandinav kebabı, yemezsen üzülürüm hesabı','Bagcilar Merkez','10:00:00','22:00:00','2','Aci Sos, Lavas ','Bar/Lounge, Beer, Cocktails, Corkage Fee,','2',0,0,'tZSnxfBc');
+
+INSERT INTO `rest_signup` VALUES(1,'lucashood','Lucas','Hood','Hood to Shoot','hood@gmail.com','c48441e989575be5b9c68af087510134','Bebek','12345','Bebek Merkez','11:00:00','21:00:00',50);
+INSERT INTO `rest_signup` VALUES(2,'pankyrest','Ugur','Tas','Pankys Hall','ayibear@hotmail.com','c48441e989575be5b9c68af087510134','Eminonu','12345','Eminonu Pazari','11:00:00','21:00:00',670);
+INSERT INTO `rest_signup` VALUES(3, 'sinopmanti','Ali','Atahan','Meshur Sinop Manticisi','57sinob@gmail.com','c48441e989575be5b9c68af087510134','Sultanbeyli','12345','Sultanbeyli Merkez','11:00:00','21:00:00',150);
+INSERT INTO `rest_signup` VALUES(4,'ozkan58','Oguz','Ozkan','Sivasli Prens','kangal@gmail.com','c48441e989575be5b9c68af087510134','Yenidogan','12345','Yenidogan Metro','10:00:00','20:00:00',30);
+INSERT INTO `rest_signup` VALUES(5,'pismaniye41','Berke','Biten','Izmit ','izmit@gmail.com','c48441e989575be5b9c68af087510134','Tasdelen','12345','Tasdelen Saat Kulesi','8:00:00','23:00:00',110);
+
+INSERT INTO `admin` VALUES ('aykut1907','aykut','kocaman','aykut54@gmail.com','e5a88636b50c779a7353c5c93bc36900','a63kuIWe');
+INSERT INTO `admin` VALUES ('drogba1905','didier','drogba','drogba@gmail.com','e5a88636b50c779a7353c5c93bc36900','YSjTZqDP');
+INSERT INTO `admin` VALUES ('deliibo','ibrahim','uzulmez','ibo1903@gmail.com','e5a88636b50c779a7353c5c93bc36900','90rlyjS8');
+INSERT INTO `admin` VALUES ('madridli','sergio','ramos','ramos_madrid@gmail.com','e5a88636b50c779a7353c5c93bc36900','jF4Z9HOT');
+
+INSERT INTO `bookings` VALUES (1,'kelbero','lapulga',5,'20:00:00','22:00:00','Berat','Karacalar','berat54@gmail.com',05379235986,'2019-12-15');
+INSERT INTO `bookings` VALUES (2,'berbuser','kingjames',1,'15:00:00','16:00:00','Berke','Biten','beko@gmail.com',05373215346,'2019-12-26');
+INSERT INTO `bookings` VALUES (3,'kelbero','allfather',3,'10:00:00','12:00:00','Berat','Karacalar','berat54@gmail.com',05379235986,'2019-12-20');
+INSERT INTO `bookings` VALUES (4,'kelbero','lapulga',2,'20:00:00','23:00:00','Berat','Karacalar','berat54@gmail.com',05379235986,'2019-12-24');
+
+INSERT INTO `review` VALUES (1,'kelbero','lapulga','Cok guzel bir aksam gecirdim harika bi mekan burası.',3,2,'Tesekkurler.');
+INSERT INTO `review` VALUES (2,'kelbero','allfather','Dunyanin en guzel restorani olabilir.Tatlilarini ozellikle tavsiye ederim.',2,1,'Yine bekleriz.');
+
+INSERT INTO ticket(user_uname,category, description, date, isResponded) VALUES ('pankylol','help','help me','2019-12-17',0);
+INSERT INTO ticket(user_uname,category, description, date, isResponded) VALUES ('pankylol','help','help me please','2019-12-18',0);
+INSERT INTO ticket(user_uname,category,description, date, isResponded, respond, admin_uname) VALUES ('kelbero','suggestion','u can add something','2019-12-12',1, 'yes we can', 'drogba1905');
+INSERT INTO ticket(rest_uname, category, description, date, isResponded) VALUES ('allfather', 'suggestion', 'i need this', '2019-12-20', 0);
