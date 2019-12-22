da9c7d95edd75b40b1e9911604f14596c722d023
berkebiten <xqaesocial@gmail.com>
2019-12-22 19:18:15 +0300

    mostPop

--- DOCS/Implementation/Project/index.php
+++ DOCS/Implementation/Project/index.php
@@ -18,6 +18,10 @@ if (isset($_SESSION['username'])) {
         header('location:RestaurantOwner.php');
     }
 }
+
+
+$sqlMost = "SELECT restaurant_uname, COUNT(*) FROM bookings GROUP BY restaurant_uname ORDER BY COUNT(*) DESC LIMIT 3";
+$queryMost = mysqli_query($conn, $sqlMost);
 ?>
 
 
@@ -33,7 +37,7 @@ if (isset($_SESSION['username'])) {
         <?php if (isset($_SESSION['success'])): ?>
 
             <a href="SignOut.php"><button  id="signout">Sign Out </button></a>
-            <a href='userProfile.php?varname=<?php echo $_SESSION['username']?>'><button id="profile" ><?php echo $_SESSION['username'] ?></button></a>
+            <a href='userProfile.php?varname=<?php echo $_SESSION['username'] ?>'><button id="profile" ><?php echo $_SESSION['username'] ?></button></a>
             <a href ="support.php"><button id ="support"> Support</button> </a>
             <a href="index.php"><img src="img/LOGO.png" alt="RBS" style="width:150px"></a>
 
@@ -52,135 +56,47 @@ if (isset($_SESSION['username'])) {
 
     <div  class="searchpart">
         <form class="searchForm" action="searchResultGuest.php" method="post">
-<!--            <select class="searchinputs" required>
-                <option value="0"> Party Size </option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-            </select>
-            <input onclick="dateConstraint()" type="date" name="date" class="searchinputs" required/>-->
             <input type="text" class="searchinputs" name="rName" placeholder="Restaurant Name or Location.." required/>
             <input type="submit" id="searchButton" value="SEARCH">
         </form>  
     </div>
 
 
-    <h2>MOST POPULAR LOCATIONS</h2>
+    <h2>MOST POPULAR RESTAURANTS</h2>
 
-    <div class="mostpopular">  
-        <a href="restpage.asp">
-            <img src="img/kadikoy.jpg" style="width:275px;height:200px;border:0;">
-        </a>
-        <a href="restpage.asp">
-            <img src="img/uskudar.jpg" style="width:275px;height:200px;border:0;">
-        </a>
+    <div class="mostpopular">
+        <?php $count = 0; ?>
+        <?php while ($arrMost = mysqli_fetch_array($queryMost, MYSQLI_ASSOC)): ?>
+            <div class="cardPlace" > 
+                <?php
+                $sql9 = "select * from restaurant_owner where uname='" . $arrMost['restaurant_uname'] . "'";
+                $query19 = mysqli_query($conn, $sql9);
+                $arr19 = mysqli_fetch_array($query19, MYSQLI_ASSOC);
+                $restaurantName = $arr19['rest_name'];
+                $location = $arr19['location'];
+                $capacity = $arr19['cap'];
+                $phoneNo = $arr19['phoneNo'];
+                $description = $arr19['description'];
+                ?>
 
-        <a href="restpage.asp">
-            <img src="img/besiktas.jpg" style="width:275px;height:200px;border:0;">
-        </a><br></br>
-        <a href="restpage.asp">
-            <img src="img/eminonu.jpg" style="width:275px;height:200px;border:0;">
-        </a>
-        <a href="restpage.asp">
-            <img src="img/sariyer.jpg" style="width:275px;height:200px;border:0;">
-        </a>
-        <a href="restpage.asp">
-            <img src="img/eyup.jpg" style="width:275px;height:200px;border:0;">
-        </a>
+                <div class="restaurantCard">
+                    <a href="restaurantProfile.php?varname=<?php echo $arrMost['restaurant_uname'] ?>"><h2><?php echo $restaurantName ?></h2></a>
+                    <div class="cardContainer">
+                        <h4><?php echo $description ?></h4>
+                        <h4>Location: <?php echo $location ?></h4>
+                        <h4>Phone: <?php echo $phoneNo ?></h4>
+                    </div>
+                </div>
+
+                
+            </div>
+        <?php endwhile ?>
+
+
     </div>
 
 </div>
 
-<!--<div class="form-popup" id="signIn">
-    <form method="post" class="form-container" action="signIn.php">
-        <h3>Sign In</h3>
-        <label for="username"><b>Username</b></label>
-        <input type="text" placeholder="  Enter Username" name="username" required/>
-        <br><br>
-        <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="  Enter Password" name="psw" required/>
-        <span class="forgotpsw"> <a href="forgotPsw.php" onclick="openForm4()">Forgot password?</a></span>
-        <br><br>
-        <button type="submit" class="btn">Login</button>
-        <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
-    </form>
-</div>
-
-
-<div class = "form-popup" id="signxp">
-    <form method="post" class="form-container" >
-        <h3>Sign Up</h3>
-        <label for="fname">First Name</label>
-        <input placeholder="Enter first name" type="text" name="fname" required/>
-        <br><br>
-        <label for="lname">Last Name</label>
-        <input placeholder="Enter last name" type="text" name="lname" required/>
-        <br><br>
-        <label for="uname">Username</label>
-        <input placeholder="Enter username" type="text" name="uname" pattern=".{6,30}" title="between 6 and 30 characters" required/>
-        <br><br>
-        <label for="email">E-Mail</label>
-        <input placeholder="Enter email" type="email" name="email"  required/>
-        <br><br>
-        <label for="psw">Password</label>
-        <input placeholder="Enter password" type="password" name="psw" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
-               title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required/>
-        <button type="button" id="cancel" onclick="closeForm()">Cancel</button>
-        <input type="submit" id = "ca" value="Create Account">
-    </form>
-</div>
-
-<div class="form-popup" id="restSignUp">
-    <form method="post" class="form-container" action="restSignUp.php"> <h3>Restaurant Sign Up</h3>
-        <label for="fname">First Name</label>
-        <input placeholder="Enter first name" class="rSign" type="text" name="fname" required/>
-        <br><br>
-        <label for="lname">Last Name</label>
-        <input placeholder="Enter last name" class="rSign" type="text" name="lname" required/>
-        <br><br>
-        <span></span>
-        <label for="uname">Username</label>
-        <input placeholder="Enter username" type="text" name="uname" pattern=".{6,30}" title="between 6 and 30 characters" required/>
-        <br><br>
-        <label for="uname">Restaurant Name</label>
-        <input placeholder="Enter restaurant name" class="rSign" type="text" name="rname" required/>
-        <br><br>
-        <label for="email">E-Mail</label>
-        <input placeholder="Enter email" class="rSign" type="email" name="email" required/>
-        <br><br>
-        <label for="psw">Password</label>
-        <input placeholder="Enter password" class="rSign" type="password" name="psw" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
-               title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required/>
-        <label for="uname">Phone No</label>
-        <input placeholder="Enter phone no" class="rSign" type="text" name="phone" required/>
-        <br><br>
-        <label for="uname">Adress</label>
-        <input placeholder="Enter address" class="rSign" type="text" name="adress" required/>
-        <br><br>
-        <label for="cap">Restaurant Capacity</label>
-        <input placeholder="Capacity" class="rSign" type="number" name="cap" min="1" required/>
-        <br><br>
-        <button type="button" id="cancel" onclick="closeForm()">Cancel</button>
-        <input type="submit" id = "ca" value="Create Account">
-    </form>
-
-</div>
-
-<div class = "form-popup" id="forgotpsw">
-    <form method="post" class="form-container">
-        <label for="cap">Enter your recovery code and your password will reset.</label>
-        <input type="text" placeholder="Recovery Code"></input>
-        <button type="button" id="cancel" onclick="closeForm()">Cancel</button>
-        <button type="button" id="ca">Send</button>
-    </form>
-</div>-->
 </body>
 </html>
 
