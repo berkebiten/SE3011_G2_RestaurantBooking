4cc383f722398e7a7a3cd69292d3341ef6cd17a3
VolkanDavsan <57091151+VolkanDavsan@users.noreply.github.com>
2019-12-22 19:32:52 +0300

    editRestaurant

---
+++ DOCS/Implementation/Project/editMyProfile.php
@@ -0,0 +1,124 @@
+<link rel="stylesheet" href="style.css"></link>
+<?php
+include 'editMyProfileProcess.php';
+if (isset($_SESSION['username'])) {
+    $usercheck2 = $_SESSION['username'];
+     $sql3 = "select uname from restaurant_owner where uname='$usercheck2'";
+       $queryR = mysqli_query($conn, $sql3);
+        $isOwnerViewing = false;
+        if (mysqli_num_rows($queryR) > 0) {
+        $isOwnerViewing = true;
+    }
+      if(!$isOwnerViewing){
+        header('location:errorPage.php');
+    }
+}
+$uname = $_GET['varname'];
+$sql45 = "select * from restaurant_owner where uname='$uname'";
+$query2 = mysqli_query($conn, $sql45);
+$editArray = mysqli_fetch_array($query2, MYSQLI_ASSOC);
+$fname = $editArray['fname'];
+$lname = $editArray['lname'];
+$restName = $editArray['rest_name'];
+$location = $editArray['location'];
+$phone = $editArray['phoneNo'];
+$cap = $editArray['cap'];
+$description = $editArray['description'];
+$payment = $editArray['payment'];
+$additional = $editArray['additional'];
+$address = $editArray['address'];
+$startTime = $editArray['startTime'];
+$endTime = $editArray['endTime'];
+$cuisines = $editArray['cuisines'];
+$seating_options = $editArray['seating_options'];
+$price = $editArray['price'];
+$email = $editArray['email'];
+?>
+<body>
+<div class="top">
+
+
+    <a href="SignOut.php"><button  id="signout">Sign Out </button></a>
+    <a href='restaurantProfile.php?varname=<?php echo $_SESSION['username'] ?>'><button id="profile" ><?php echo $_SESSION['username'] ?></button></a>
+    <a href ="support.php"><button id ="support"> Support</button> </a>
+    <a href="index.php"><img src="img/LOGO.png" alt="RBS" style="width:150px"></a>
+
+
+</div>
+<div class="container" id="fullC">
+    <div id='formArea'>
+        <?php include('errors.php'); ?>
+        <form class='formX' method='post' action='editMyProfile.php?varname=<?php echo $uname ?>'>
+               <div class="input-group">
+                <label for="fname">First Name</label>
+                <input class="input" type="text" placeholder="Enter First Name"name="fname" value="<?php echo $fname ?>"></input>
+            </div>
+              <div class="input-group">
+                <label for="lname">Last Name</label>
+                <input class="input" type="text"  placeholder="Enter Last Name"name="lname" value="<?php echo $lname ?>"></input>
+            </div>
+            <div class="input-group">
+                <label for="rName">Restaurant Name</label>
+                <input class="input" type="text" value="<?php echo $restName ?>" name="rName"></input>
+            </div>
+             <div class="input-group">
+                <label for="location">Location</label>
+                <input class="input" type="text" value="<?php echo $location?>" name="location"></input>
+            </div>
+             <div class="input-group">
+                <label for="phone">Phone Number</label>
+                <input class="input" type="text" value="<?php echo $phone ?>" name="phone"></input>
+            </div>
+             <div class="input-group">
+                <label for="capacity">Capacity</label>
+                <input class="input" type="text" value="<?php echo $cap ?>" name="capacity"></input>
+            </div>
+            <div class="input-group">
+                <label for="description">Description</label>
+            <textarea rows="8" cols="50" class="tArea" name="description"><?php echo $description ?></textarea>
+            </div>
+             <div class="input-group">
+                <label for="payment">Payment</label>
+                <input class="input" type="text" value="<?php echo $payment?>" name="payment"></input>
+            </div>
+                <div class="input-group">
+                <label for="additional">Additional</label>
+                <input class="input" type="text" value="<?php echo $additional?>" name="additional"></input>
+            </div>
+            <div class="input-group">
+                <label for="address">Address</label>
+                <input class="input" type="text" value="<?php echo $address?>" name="address"></input>
+            </div>
+          
+            <div class="input-group">
+                <label for="time">Start Time</label>
+                <input class="input" id="bookingsTime" type="time" name="startTime" value="<?php echo $startTime ?>"> </input>
+            </div>
+            <div class="input-group">
+                <label for="time">End Time</label>
+                <input class="input" id="bookingeTime" type="time" name="endTime" value="<?php echo $endTime ?>"></input>
+            </div>
+              <div class="input-group">
+                <label for="cuisines">Cuisines</label>
+                <input class="input" type="text" value="<?php echo $cuisines?>" name="cuisines"></input>
+            </div>
+              <div class="input-group">
+                <label for="seating_options">Seating Options</label>
+                <input class="input" type="text" value="<?php echo $seating_options?>" name="seating_options"></input>
+            </div>
+            <div class="input-group">
+                <label for="price">Price</label>
+                <input class="input" type="text" value="<?php echo $price?>" name="price"></input>
+            </div>
+            <a href='editMyProfileProcess.php?varname=<?php echo $uname ?>'><button type='submit' class='btn' name='editRestaurant'>Edit</button></a>
+        </form>   
+    </div>
+    <div  id="feedback">
+        <?php include('feedbacks.php') ?>
+        <?php if (count($feedbacks) > 0) : ?>
+            <script> openFeedback();</script>
+            <button onclick="window.location.href = 'restaurantProfile.php?varname=<?php echo $uname ?>'" >OK</button>
+        <?php endif ?>
+    </div>
+</div>
+</body>
---
+++ DOCS/Implementation/Project/editMyProfileProcess.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="style.css"></link>
+<script src="scripts.js"></script>
+<?php
+session_start();
+include("dbconnect.php");
+$feedbacks = array();
+$errors = array();
+//$count = mysqli_num_rows($query);
+//if($count == 0) {
+//    header('location:errorPage.php');
+//}
+
+if (isset($_POST['editRestaurant'])) {
+    $uname = $_SESSION['username'];
+    $fname = mysqli_real_escape_string($conn, $_POST['fname']);
+    $lname = mysqli_real_escape_string($conn, $_POST['lname']);
+    $rest_name = mysqli_real_escape_string($conn, $_POST['rName']);
+    $location = mysqli_real_escape_string($conn, $_POST['location']);
+    $phone = mysqli_real_escape_string($conn, $_POST['phone']);
+    $capacity = mysqli_real_escape_string($conn, $_POST['capacity']);
+    $description = mysqli_real_escape_string($conn, $_POST['description']);
+    $payment = mysqli_real_escape_string($conn, $_POST['payment']);
+    $additional = mysqli_real_escape_string($conn, $_POST['additional']);
+    $address = mysqli_real_escape_string($conn, $_POST['address']);
+    $startTime = mysqli_real_escape_string($conn, $_POST['startTime']);
+    $endTime = mysqli_real_escape_string($conn, $_POST['endTime']);
+    $cuisines = mysqli_real_escape_string($conn, $_POST['cuisines']);
+    $seatingOptions = mysqli_real_escape_string($conn, $_POST['seating_options']);
+      $price = mysqli_real_escape_string($conn, $_POST['price']);
+
+    if (empty($fname)) {
+        array_push($errors, "First Name is required");
+    }
+    if (empty($lname)) {
+        array_push($errors, "Last Name is required");
+    }
+    if (empty($rest_name)) {
+        array_push($errors, "Restaurant Name is required");
+    }
+    if (empty($location)) {
+        array_push($errors, "Location is required");
+    }
+    if (empty($phone)) {
+        array_push($errors, "Phone is required");
+    }
+    if (empty($capacity)) {
+        array_push($errors, "Capacity is required. ");
+    }
+    if (empty($description)) {
+        array_push($errors, "Description is required");
+    }
+    if (empty($payment)) {
+        array_push($errors, "Payment is required");
+    }
+     if (empty($additional)) {
+        array_push($errors, "Additional is required");
+    }
+     if (empty($address)) {
+        array_push($errors, "Address is required");
+    }
+     if (empty($startTime)) {
+        array_push($errors, "Start time is required");
+    }
+     if (empty($endTime)) {
+        array_push($errors, "End time is required");
+    }
+     if (empty($cuisines)) {
+        array_push($errors, "Cuisine is required");
+    }
+     if (empty($seatingOptions)) {
+        array_push($errors, "Seating option is required");
+    }
+     if (empty($price)) {
+        array_push($errors, "Price is required");
+    }
+
+
+    if (count($errors) == 0) {
+         $uname = $_SESSION['username'];
+       $queryEdit = "UPDATE restaurant_owner SET fname='$fname', lname='$lname', rest_name='$rest_name', location='$location', phoneNo='$phone', cap='$capacity', description='$description', payment='$payment', additional='$additional', address ='$address', startTime='$startTime', endTime='$endTime', cuisines='$cuisines', seating_options='$seatingOptions', price='$price' WHERE uname='$uname' ";
+      mysqli_query($conn,$queryEdit);
+        array_push($feedbacks, "Your restaurant has been editted.");
+        array_push($feedbacks, "You will be redirected to Restaurant Page when you click 'OK' button.");
+    } else {
+        array_push($errors, "Something wrong with the edit.");
+    }
+}
+?>
+
