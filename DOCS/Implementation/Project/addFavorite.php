6ba8b6cf604d528281d4e736dfa545ecb0949b9c
OguzKaanYazan <56766209+OguzKaanYazan@users.noreply.github.com>
2019-12-23 17:18:45 +0300

    favoriteFix

--- DOCS/Implementation/Project/addFavorite.php
+++ DOCS/Implementation/Project/addFavorite.php
@@ -1,13 +1,14 @@
 <?php
 include('dbconnect.php');
 session_start();
-$usercheck = $_SESSION['username'];
-$sql = "select uname from user where uname='$usercheck'";
+$usercheck = $_SESSION['username']; //SESSION USERNAME
+$sql = "select uname from user where uname='$usercheck'"; 
 $query = mysqli_query($conn,$sql);
-$row = mysqli_num_rows($query);
+$row = mysqli_num_rows($query); 
+//CHECK THE VIEWER IS USER OR NOT
 if($row != 1){
-    header('location:errorPage.php');
-}else {
+    header('location:errorPage.php'); //IF VIEWER IS NOT USER REDIRECT TO ERROR PAGE
+}else { //ELSE ADD TO FAVORITES THE RESTAURANT
     $id = null;
     $rest= $_GET['varname'];
     $addFavorite = "insert into favorites values('$id','$usercheck','$rest')";
--- DOCS/Implementation/Project/removeFavorite.php
+++ DOCS/Implementation/Project/removeFavorite.php
@@ -1,16 +1,17 @@
 <?php
 include('dbconnect.php');
 session_start();
-$usercheck = $_SESSION['username'];
-$sql = "select uname from user where uname='$usercheck'";
+$usercheck = $_SESSION['username']; //SESSION USERNAME
+$id= $_GET['varname']; //FAVORITE ID
+$sql = "select * from favorites where favoritesId='$id' and customer_uname='$usercheck'";
 $query = mysqli_query($conn,$sql);
 $row = mysqli_num_rows($query);
+//CHECK IF THE VIEWER TRY TO REMOVE HIS/HER OWN FAVORITE OR NOT
 if($row != 1){
-    header('location:errorPage.php');
+    header('location:errorPage.php'); //IF SOMEONE ELSE TRY TO REMOVE, REDIRECT TO ERROR PAGE
 }else {
-    $id= $_GET['varname'];
     $removeFavorite = "delete from favorites where favoritesId='$id'";
     $removeFQ = mysqli_query($conn,$removeFavorite);
-    header('location:userProfile.php?varname='.$_SESSION['username']);
+    header('location:userProfile.php?varname='.$usercheck);
 }
 
