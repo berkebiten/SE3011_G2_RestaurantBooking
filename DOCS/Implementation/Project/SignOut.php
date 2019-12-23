d7e8cf38d8d672adace86f31224fc046aa7cf06c
berkebiten <xqaesocial@gmail.com>
2019-12-23 19:15:11 +0300

    Update SignOut.php

--- DOCS/Implementation/Project/SignOut.php
+++ DOCS/Implementation/Project/SignOut.php
@@ -1,8 +1,9 @@
 <link rel="stylesheet" href="style.css"></link>
 <script src="scripts.js"></script>
 <?php
+
 session_start();
-session_destroy();
+session_destroy();//DESTROYS ALL OF THE SESSION'S VARIABLES
 header('location:index.php');
 ?>
 
