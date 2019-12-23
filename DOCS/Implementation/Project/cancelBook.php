2289218c6b8cfb6bf4fa5a59bd560b5890a03e78
berkebiten <xqaesocial@gmail.com>
2019-12-23 23:27:07 +0300

    Update cancelBook.php

--- DOCS/Implementation/Project/cancelBook.php
+++ DOCS/Implementation/Project/cancelBook.php
@@ -1,26 +1,27 @@
 <?php
+
 session_start();
 include 'dbconnect.php';
-if(!isset($_SESSION['username'])){
-	header('location:errorPage.php');
-}
-else{
-
-
-$uname = $_SESSION['username'];
-$vname = $_GET['varname']; 
-$sql1 = "select * from bookings where bookingId='$vname'";
-$query1 = mysqli_query($conn, $sql);
-$arr1 = mysqli_fetch_arry($query1, MYSQLI_ASSOC);
-if(mysqli_num_rows($arr1)>0){
-	$customer_uname = $arr1['customer_uname'];
-	$restaurant_uname = $arr1['customer_uname'];
-	$sql = "delete from bookings where bookingId='$vname'";
-$query = mysqli_query($conn, $sql);
-header("location: viewMyBookings.php?varname=$uname");
-}else{
-	header('location:errorPage.php');
-}
-
+if (!isset($_SESSION['username'])) {
+    header('location:errorPage.php');//REDIRECTS TO ERRORPAGE IF SESSION DOES NOT EXIST
+} else {
+    $uname = $_SESSION['username'];
+    $vname = $_GET['varname'];
+    $sql1 = "select * from bookings where bookingId='$vname'";
+    $query1 = mysqli_query($conn, $sql1);
+    $arr1 = mysqli_fetch_array($query1, MYSQLI_ASSOC);
+    if (mysqli_num_rows($arr1) > 0) {
+        $customer_uname = $arr1['customer_uname'];
+        $restaurant_uname = $arr1['customer_uname'];
+        if ($customer_uname != $_SESSION['username']) {//CHECKS IF THE CALLER IS THE OWNER OF THE BOOKING
+            header('location: errorPage.php'); //REDIRECTS TO ERROR PAGE IF NOT
+        } else {
+            $sql = "delete from bookings where bookingId='$vname'";//DELETE THE BOOKING FROM DATABASE
+            $query = mysqli_query($conn, $sql);
+            header("location: viewMyBookings.php?varname=$uname");
+        }
+    } else {
+        header('location:errorPage.php');
+    }
 }
 ?>
