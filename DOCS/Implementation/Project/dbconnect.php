35aa2f07deeaabe1086c6a756b3aa19c2a2a2995
Ali Atahan Topal <atahanali57@gmail.com>
2019-12-23 18:25:54 +0300

    Update dbconnect.php

--- DOCS/Implementation/Project/dbconnect.php
+++ DOCS/Implementation/Project/dbconnect.php
@@ -1,12 +1,12 @@
 <?php
-$dbname = "rbs";
-$conn = mysqli_connect("localhost", "root","");
+$dbname = "rbsdb"; // database name
+$conn = mysqli_connect("localhost", "root",""); //establishing connection with the database 
 if (!$conn) {
-    die('not connected:'.mysqli_error());
+    die('not connected:'.mysqli_error()); // it is the error that is given after a failed database connection
 }
-$b = mysqli_select_db($conn, $dbname);
+$b = mysqli_select_db($conn, $dbname); // selecting the database schema
 if (!$b) {
-    die("db couldn't retrieved".mysqli_error($dbname));
+    die("db couldn't retrieved".mysqli_error($dbname)); // its the error that is given after a failed retrieval of a database schema
 }
 
 ?>
